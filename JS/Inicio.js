7e0fa0037863d8af34198374aaca68331e7ec621
FabricioSalgueiro <salgueirofabricio6@gmail.com>
2025-11-10 11:51:10 -0300

    Arreglos visuales en home

--- JS/Inicio.js
+++ JS/Inicio.js
@@ -1,8 +1,9 @@
+// botones del carrusel populares
+
 const carrusel = document.querySelector('.carrusel');
 const btnPrev = document.querySelector('.prev');
 const btnNext = document.querySelector('.next');
 
-// Cantidad de desplazamiento (igual al ancho de un .juego)
 let desplazamiento = 750; 
 
 btnNext.addEventListener('click', () => {
@@ -16,6 +17,21 @@ btnPrev.addEventListener('click', () => {
   carrusel.scrollBy({
     left: -desplazamiento,
     behavior: 'smooth'
+  });
+});
+
+
+// Deco del hover
+
+document.addEventListener("DOMContentLoaded", () => {
+  const juegos = document.querySelectorAll('.juegoCatalogo');
+
+  juegos.forEach(juego => {
+    const img = juego.querySelector('img');
+    const texto = document.createElement('div');
+    texto.classList.add('name-game');
+    texto.textContent = img.alt; // usa el alt como texto visible
+    juego.appendChild(texto);
   });
 });
 
--- Views/home.php
+++ Views/home.php
@@ -1,12 +1,14 @@
 <!-- Juegos populares / destacados -->
 
-<div class="JuegosPopulares">
-  <h1>Juegos populares</h1>
 
-  <div class="contenedorPrincipal">
-    <button class="btn-carrusel prev">❮</button>
+
+<div class="JuegosPopulares">
+  <div class="contenedorPopulares">
+  <h1 class="title-popular-games">Juegos populares</h1>
 
     <div class="content carrusel">
+      
+       <button class="btn-carrusel prev">❮</button>
       <a href="Mainsite.php?section=selectedgame&id=5">
         <div class="juego" style="background-image: url('../IMG/IconoJuego.jpg');"></div>
       </a>
@@ -23,81 +25,136 @@
   </div>
 </div>
 
-<!-- Catálogo completo -->
-<div class="MasJuegos">
-  <h1>Catálogo de juegos</h1>
-
+<!-- Juegos de accion papa -->
+<div class="JuegosCatalogo">
+  <h1 class="title-action-games">Juegos de acción</h1>
+  <!-- <button class="carruselPopulares1">❮</button> -->
   <div class="contenedorCatalogo">
-    <a href="Mainsite.php?section=selectedgame&id=1">
+    <a href="Mainsite.php?section=selectedgame&id=5">
       <div class="juegoCatalogo">
-        <img src="../IMG/JuegoRandom1.webp" alt="Juego 1">
+        <img src="../IMG/IconoJuego.jpg" alt="Station Defenders">
       </div>
     </a>
     <a href="Mainsite.php?section=selectedgame&id=2">
       <div class="juegoCatalogo">
-        <img src="../IMG/JuegoRandom.webp" alt="Juego 2">
+        <img src="../IMG/JuegoRandom.webp" alt="Minecraft vs Roblox">
       </div>
     </a>
     <a href="Mainsite.php?section=selectedgame&id=3">
       <div class="juegoCatalogo">
-        <img src="../IMG/JuegoRandom2.webp" alt="Juego 3">
+        <img src="../IMG/JuegoRandom2.webp" alt="Esquivar">
       </div>
     </a>
     <a href="Mainsite.php?section=selectedgame&id=4">
       <div class="juegoCatalogo">
-        <img src="../IMG/JuegoRandom3.jpg" alt="Juego 4">
+        <img src="#" alt="Mario vs Sonic">
       </div>
     </a>
     <a href="Mainsite.php?section=selectedgame&id=5">
       <div class="juegoCatalogo">
-        <img src="../IMG/IconoJuego.jpg" alt="Juego 5">
+        <img src="#" alt="Spaceship">
       </div>
     </a>
     <a href="Mainsite.php?section=selectedgame&id=6">
       <div class="juegoCatalogo">
-        <img src="../IMG/JuegoRandom5.webp" alt="Juego 6">
+        <img src="#" alt="Pong">
       </div>
     </a>
     <a href="Mainsite.php?section=selectedgame&id=7">
       <div class="juegoCatalogo">
-        <img src="../IMG/JuegoRandom6.png" alt="Juego 7">
+        <img src="#" alt="Golf">
       </div>
     </a>
-    <a href="Mainsite.php?section=selectedgame&id=8">
-      <div class="juegoCatalogo">
-        <img src="../IMG/JuegoRandom7.jpg" alt="Juego 8">
-      </div>
-    </a>
-    <a href="Mainsite.php?section=selectedgame&id=9">
-      <div class="juegoCatalogo">
-        <img src="../IMG/JuegoRandom8.jpg" alt="Juego 9">
-      </div>
-    </a>
-    <a href="Mainsite.php?section=selectedgame&id=10">
-      <div class="juegoCatalogo">
-        <img src="../IMG/JuegoRandom9.jpg" alt="Juego 10">
-      </div>
-    </a>
-    <a href="Mainsite.php?section=selectedgame&id=11">
-      <div class="juegoCatalogo">
-        <img src="../IMG/JuegoRandom10.jpg" alt="Juego 11">
-      </div>
-    </a>
-    <a href="Mainsite.php?section=selectedgame&id=12">
-      <div class="juegoCatalogo">
-        <img src="../IMG/JuegoRandom11.jpg" alt="Juego 12">
-      </div>
-    </a>
-    <a href="Mainsite.php?section=selectedgame&id=13">
-      <div class="juegoCatalogo">
-        <img src="../IMG/JuegoRandom12.jpg" alt="Juego 13">
-      </div>
-    </a>
-    <a href="Mainsite.php?section=selectedgame&id=14">
-      <div class="juegoCatalogo">
-        <img src="../IMG/JuegoRandom13.jpg" alt="Juego 14">
-      </div>
-    </a>
+    <!-- <button class="carruselPopulares2">❯</button> -->
+  </div>
+</div>
 
+<!-- Juegos de agilidad -->
+
+<div class="JuegosCatalogo">
+  <h1 class="title-action-games">Juegos para pensar</h1>
+  <!-- <button class="carruselPopulares1">❮</button> -->
+  <div class="contenedorCatalogo">
+    <a href="Mainsite.php?section=selectedgame&id=1">
+      <div class="juegoCatalogo">
+        <img src="../IMG/JuegoRandom1.webp" alt="Truco">
+      </div>
+    </a>
+    <a href="Mainsite.php?section=selectedgame&id=2">
+      <div class="juegoCatalogo">
+        <img src="../IMG/JuegoRandom7.png" alt="Ajedrez">
+      </div>
+    </a>
+    <a href="Mainsite.php?section=selectedgame&id=3">
+      <div class="juegoCatalogo">
+        <img src="#" alt="Find The Character">
+      </div>
+    </a>
+    <a href="Mainsite.php?section=selectedgame&id=7">
+      <div class="juegoCatalogo">
+        <img src="#" alt="jaja">
+      </div>
+    </a>
+    <a href="Mainsite.php?section=selectedgame&id=5">
+      <div class="juegoCatalogo">
+        <img src="#" alt="Spaceship">
+      </div>
+    </a>
+    <a href="Mainsite.php?section=selectedgame&id=6">
+      <div class="juegoCatalogo">
+        <img src="#" alt="Pong">
+      </div>
+    </a>
+    <a href="Mainsite.php?section=selectedgame&id=7">
+      <div class="juegoCatalogo">
+        <img src="#" alt="Golf">
+      </div>
+    </a>
+    <!-- <button class="carruselPopulares2">❯</button> -->
+  </div>
+</div>
+
+<!-- Juegos de Mesa -->
+
+<div class="JuegosCatalogo">
+  <h1 class="title-action-games">Juegos de mesa</h1>
+  <!-- <button class="carruselPopulares1">❮</button> -->
+  <div class="contenedorCatalogo">
+    <a href="Mainsite.php?section=selectedgame&id=1">
+      <div class="juegoCatalogo">
+        <img src="../IMG/JuegoRandom1.webp" alt="Truco">
+      </div>
+    </a>
+    <a href="Mainsite.php?section=selectedgame&id=2">
+      <div class="juegoCatalogo">
+        <img src="../IMG/JuegoRandom8.png" alt="Ludo">
+      </div>
+    </a>
+    <a href="Mainsite.php?section=selectedgame&id=3">
+      <div class="juegoCatalogo">
+        <img src="../IMG/JuegoRandom3.jpg" alt="Solitario">
+      </div>
+    </a>
+    <a href="Mainsite.php?section=selectedgame&id=7">
+      <div class="juegoCatalogo">
+        <img src="../IMG/JuegoRandom7.png" alt="Ajedrez">
+      </div>
+    </a>
+    <a href="Mainsite.php?section=selectedgame&id=5">
+      <div class="juegoCatalogo">
+        <img src="../IMG/JuegoRandom9.png" alt="Damas">
+      </div>
+    </a>
+    <a href="Mainsite.php?section=selectedgame&id=6">
+      <div class="juegoCatalogo">
+        <img src="../IMG/JuegoRandom10.jpeg" alt="Poker">
+      </div>
+    </a>
+    <a href="Mainsite.php?section=selectedgame&id=7">
+      <div class="juegoCatalogo">
+        <img src="#" alt="Ajedrez">
+      </div>
+    </a>
+    <!-- <button class="carruselPopulares2">❯</button> -->
   </div>
 </div>
